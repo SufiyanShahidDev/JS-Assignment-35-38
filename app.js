4477fd1962e675fe3a16a1716c6ea7f401691c05
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-19 12:59:05 +0500

    Initial commit

--- app.js
+++ app.js
@@ -11,18 +11,34 @@
 
 // Answer2
 
-function greetUser() {
-    var firstName = prompt("Enter your first name:");
-    var lastName = prompt("Enter your last name:");
+// function greetUser() {
+//     var firstName = prompt("Enter your first name:");
+//     var lastName = prompt("Enter your last name:");
 
-    // Capitalize first letter of each name
-    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
-    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+//     // Capitalize first letter of each name
+//     firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+//     lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
 
-    var fullName = firstName + " " + lastName;
+//     var fullName = firstName + " " + lastName;
 
-    alert("Hello " + fullName);
-    console.log("Hello " + fullName);
+//     alert("Hello " + fullName);
+//     console.log("Hello " + fullName);
+// }
+
+// greetUser();
+
+// Answer3
+
+function addNumbers() {
+    var num1 = +prompt("Enter first number:");
+    var num2 = +prompt("Enter second number:");
+
+    var sum = num1 + num2;
+
+    alert("Sum is: " + sum);
+    console.log("Sum is:", sum);
+
+    return sum;
 }
 
-greetUser();
+addNumbers();
