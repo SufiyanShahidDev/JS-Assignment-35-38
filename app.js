230188db7f564e77244b8883edab10a6defe4f8b
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-19 12:45:46 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+// Answer1
+
+function showDateTime() {
+    var now = new Date();
+    alert("Current Date & Time:\n" + now);
+    console.log("Current Date & Time:", now);
+}
+
+showDateTime();
