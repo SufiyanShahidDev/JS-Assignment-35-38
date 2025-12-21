41e9533a898fc9dac3dc6f0a2c81d033b02daf36
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2025-12-21 21:31:27 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,233 +1,233 @@
 
 // Answer1
 
-// function showDateTime() {
-//     var now = new Date();
-//     alert("Current Date & Time:\n" + now);
-//     console.log("Current Date & Time:", now);
-// }
+function showDateTime() {
+    var now = new Date();
+    alert("Current Date & Time:\n" + now);
+    console.log("Current Date & Time:", now);
+}
 
 // showDateTime();
 
 // Answer2
 
-// function greetUser() {
-//     var firstName = prompt("Enter your first name:");
-//     var lastName = prompt("Enter your last name:");
+function greetUser() {
+    var firstName = prompt("Enter your first name:");
+    var lastName = prompt("Enter your last name:");
 
-//     // Capitalize first letter of each name
-//     firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
-//     lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+    // Capitalize first letter of each name
+    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
 
-//     var fullName = firstName + " " + lastName;
+    var fullName = firstName + " " + lastName;
 
-//     alert("Hello " + fullName);
-//     console.log("Hello " + fullName);
-// }
+    alert("Hello " + fullName);
+    console.log("Hello " + fullName);
+}
 
-// greetUser();
+greetUser();
 
 // Answer3
 
-// function addNumbers() {
-//     var num1 = +prompt("Enter first number:");
-//     var num2 = +prompt("Enter second number:");
+function addNumbers() {
+    var num1 = +prompt("Enter first number:");
+    var num2 = +prompt("Enter second number:");
 
-//     var sum = num1 + num2;
+    var sum = num1 + num2;
 
-//     alert("Sum is: " + sum);
-//     console.log("Sum is:", sum);
+    alert("Sum is: " + sum);
+    console.log("Sum is:", sum);
 
-//     return sum;
-// }
+    return sum;
+}
 
-// addNumbers();
+addNumbers();
 
 // Answer4
 
-// function calculator() {
-//     var num1 = +prompt("Enter first number:");
-//     var num2 = +prompt("Enter second number:");
-//     var operator = prompt("Enter operator (+, -, *, /):");
+function calculator() {
+    var num1 = +prompt("Enter first number:");
+    var num2 = +prompt("Enter second number:");
+    var operator = prompt("Enter operator (+, -, *, /):");
 
-//     var result;
+    var result;
 
-//     if (operator === "+") {
-//         result = num1 + num2;
-//     }
-//     else if (operator === "-") {
-//         result = num1 - num2;
-//     }
-//     else if (operator === "*") {
-//         result = num1 * num2;
-//     }
-//     else if (operator === "/") {
-//         result = num1 / num2;
-//     }
-//     else {
-//         alert("Invalid operator");
-//         return;
-//     }
+    if (operator === "+") {
+        result = num1 + num2;
+    }
+    else if (operator === "-") {
+        result = num1 - num2;
+    }
+    else if (operator === "*") {
+        result = num1 * num2;
+    }
+    else if (operator === "/") {
+        result = num1 / num2;
+    }
+    else {
+        alert("Invalid operator");
+        return;
+    }
 
-//     alert("Result is: " + result);
-//     console.log("Result is:", result);
+    alert("Result is: " + result);
+    console.log("Result is:", result);
 
-//     return result;
-// }
+    return result;
+}
 
-// calculator();
+calculator();
 
 // Answer5
 
-// function squareNumber() {
-//     var num = +prompt("Enter a number:");
+function squareNumber() {
+    var num = +prompt("Enter a number:");
 
-//     var result = num * num;
+    var result = num * num;
 
-//     alert("Square of the number is: " + result);
-//     console.log("Square of the number is:", result);
+    alert("Square of the number is: " + result);
+    console.log("Square of the number is:", result);
 
-//     return result;
-// }
+    return result;
+}
 
-// squareNumber();
+squareNumber();
 
 // Answer6
 
-// function factorialNumber() {
-//     var num = +prompt("Enter a number:");
-//     var factorial = 1;
+function factorialNumber() {
+    var num = +prompt("Enter a number:");
+    var factorial = 1;
 
-//     for (var i = 1; i <= num; i++) {
-//         factorial = factorial * i;
-//     }
+    for (var i = 1; i <= num; i++) {
+        factorial = factorial * i;
+    }
 
-//     alert("Factorial is: " + factorial);
-//     console.log("Factorial is:", factorial);
+    alert("Factorial is: " + factorial);
+    console.log("Factorial is:", factorial);
 
-//     return factorial;
-// }
+    return factorial;
+}
 
-// factorialNumber();
+factorialNumber();
 
 // Answer7
 
-// function showCounting() {
-//     var start = +prompt("Enter start number:");
-//     var end = +prompt("Enter end number:");
+function showCounting() {
+    var start = +prompt("Enter start number:");
+    var end = +prompt("Enter end number:");
 
-//     var result = "";
+    var result = "";
 
-//     for (var i = start; i <= end; i++) {
-//         result = result + i + " ";
-//     }
+    for (var i = start; i <= end; i++) {
+        result = result + i + " ";
+    }
 
-//     alert("Counting:\n" + result);
-//     console.log("Counting:", result);
+    alert("Counting:\n" + result);
+    console.log("Counting:", result);
 
-//     return result;
-// }
+    return result;
+}
 
-// showCounting(); 
+showCounting(); 
 
 // Answer8
 
-// function calculateHypotenuse() {
+function calculateHypotenuse() {
 
-//     var base = +prompt("Enter base:");
-//     var perpendicular = +prompt("Enter perpendicular:");
+    var base = +prompt("Enter base:");
+    var perpendicular = +prompt("Enter perpendicular:");
 
-//     function calculateSquare(num) {
-//         return num * num;
-//     }
+    function calculateSquare(num) {
+        return num * num;
+    }
 
-//     var hypotenuseSquared = calculateSquare(base) + calculateSquare(perpendicular);
-//     var hypotenuse = Math.sqrt(hypotenuseSquared);
+    var hypotenuseSquared = calculateSquare(base) + calculateSquare(perpendicular);
+    var hypotenuse = Math.sqrt(hypotenuseSquared);
 
-//     alert("Hypotenuse is: " + hypotenuse);
-//     console.log("Hypotenuse is:", hypotenuse);
+    alert("Hypotenuse is: " + hypotenuse);
+    console.log("Hypotenuse is:", hypotenuse);
 
-//     return hypotenuse;
-// }
+    return hypotenuse;
+}
 
-// calculateHypotenuse();
+calculateHypotenuse();
 
 // Answer9
 
-// function calculateArea() {
-//     var width = +prompt("Enter width of rectangle:");
-//     var height = +prompt("Enter height of rectangle:");
+function calculateArea() {
+    var width = +prompt("Enter width of rectangle:");
+    var height = +prompt("Enter height of rectangle:");
 
-//     var area = width * height;
+    var area = width * height;
 
-//     alert("Area of rectangle is: " + area);
-//     console.log("Area of rectangle is:", area);
+    alert("Area of rectangle is: " + area);
+    console.log("Area of rectangle is:", area);
 
-//     return area;
-// }
+    return area;
+}
 
-// calculateArea();
+calculateArea();
 
 // Answer10
 
-// function checkPalindrome() {
-//     var word = prompt("Enter a word:");
-//     var reverseWord = "";
+function checkPalindrome() {
+    var word = prompt("Enter a word:");
+    var reverseWord = "";
 
-//     for (var i = word.length - 1; i >= 0; i--) {
-//         reverseWord = reverseWord + word[i];
-//     }
+    for (var i = word.length - 1; i >= 0; i--) {
+        reverseWord = reverseWord + word[i];
+    }
 
-//     if (word === reverseWord) {
-//         alert(word + " is a Palindrome");
-//         console.log(word + " is a Palindrome");
-//     } else {
-//         alert(word + " is NOT a Palindrome");
-//         console.log(word + " is NOT a Palindrome");
-//     }
-// }
+    if (word === reverseWord) {
+        alert(word + " is a Palindrome");
+        console.log(word + " is a Palindrome");
+    } else {
+        alert(word + " is NOT a Palindrome");
+        console.log(word + " is NOT a Palindrome");
+    }
+}
 
-// checkPalindrome();
+checkPalindrome();
 
 // Answer11
 
-// function capitalizeWords() {
-//     var text = prompt("Enter a sentence:");
-//     var words = text.split(" ");
-//     var result = "";
+function capitalizeWords() {
+    var text = prompt("Enter a sentence:");
+    var words = text.split(" ");
+    var result = "";
 
-//     for (var i = 0; i < words.length; i++) {
-//         result = result + words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
-//     }
+    for (var i = 0; i < words.length; i++) {
+        result = result + words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase() + " ";
+    }
 
-//     alert("Result: " + result);
-//     console.log("Result:", result);
+    alert("Result: " + result);
+    console.log("Result:", result);
 
-//     return result;
-// }
+    return result;
+}
 
-// capitalizeWords();
+capitalizeWords();
 
 // Answer12
 
-// function countLetter() {
-//     var text = prompt("Enter a string:");
-//     var letter = prompt("Enter a letter to count:");
-//     var count = 0;
+function countLetter() {
+    var text = prompt("Enter a string:");
+    var letter = prompt("Enter a letter to count:");
+    var count = 0;
 
-//     for (var i = 0; i < text.length; i++) {
-//         if (text[i] === letter) {
-//             count++;
-//         }
-//     }
+    for (var i = 0; i < text.length; i++) {
+        if (text[i] === letter) {
+            count++;
+        }
+    }
 
-//     alert("The letter '" + letter + "' occurs " + count + " times.");
-//     console.log("The letter '" + letter + "' occurs " + count + " times.");
+    alert("The letter '" + letter + "' occurs " + count + " times.");
+    console.log("The letter '" + letter + "' occurs " + count + " times.");
 
-//     return count;
-// }
+    return count;
+}
 
-// countLetter();
+countLetter();
 
 // Answer13
 
